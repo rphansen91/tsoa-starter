d130748726c80a9cd6c6ce8399e2ab8b2c9cbd40
Ryan Hansen <rphansen91@gmail.com>
2022-08-31 13:26:24 -0700

    Logo

--- src/docs.ts
+++ src/docs.ts
@@ -1,11 +1,25 @@
 import express, { Application } from 'express';
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi, { JsonObject } from 'swagger-ui-express';
+import { cleanEnv, str } from 'envalid';
+
+const env = cleanEnv(process.env, {
+  LOGO: str({ default: '/logo.png' }),
+});
 
 export function RegisterDocs(app: Application) {
   app.use(express.static('public'));
   app.use('/docs', swaggerUi.serve, async (_, res) => {
     const doc = await loadSwagger();
-    return res.send(swaggerUi.generateHTML(doc));
+    const html = generateHTML(doc);
+    return res.send(html);
+  });
+}
+
+function generateHTML(doc: JsonObject) {
+  return swaggerUi.generateHTML(doc, {
+    customSiteTitle: doc.info.title,
+    customCss: `.swagger-ui img { content: url(${env.LOGO}); }`,
+    customfavIcon: '/favicon.ico',
   });
 }
 
